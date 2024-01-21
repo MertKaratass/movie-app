fbe6c25177571433c661c9d7ff9c9bd0871e35b6
“red” <karatasmert255@gmail.com>
2024-01-21 16:13:49 +0300

    Update text color in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -94,7 +94,7 @@ export default function Navbar() {
                           <span
                             className={classNames(
                               active ? "bg-gray-100" : "",
-                              "block px-4 py-2 text-sm text-gray-700 cursor-pointer"
+                              "block px-4 py-2 text-sm text-gray-600 cursor-pointer"
                             )}
                             onClick={logOut}
                           >
